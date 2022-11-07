4aad3732540ba4a6e3bc5bcf0d9ee1ab9a478a8f
yogendragujar <yogendra.gujar11@gmail.com>
2022-11-07 23:21:12 +0530

    updated security for H2-console

--- Lab6_MVC_Security_StudentRegistration/src/main/java/com/greatlearning/studentregistration/security/ApplicationSecurityConfig.java
+++ Lab6_MVC_Security_StudentRegistration/src/main/java/com/greatlearning/studentregistration/security/ApplicationSecurityConfig.java
@@ -15,45 +15,45 @@ import com.greatlearning.studentregistration.service.UserDetailsServiceImpl;
 @Configuration
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return new UserDetailsServiceImpl();
 	}
-	
+
 	@Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-	
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService());
 		authProvider.setPasswordEncoder(passwordEncoder());
 		return authProvider;
-		
+
 	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authenticationProvider());
 	}
 
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
+
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-        .antMatchers("/","/student/save","/student/showFormForAdd","/student/403").hasAnyAuthority("USER","ADMIN")
-        .antMatchers("/student/showFormForUpdate","/student/delete").hasAuthority("ADMIN")
-        .anyRequest().authenticated()
-        .and()
-        .formLogin().loginProcessingUrl("/login").successForwardUrl("/student/list").permitAll()
-        .and()
-        .logout().logoutSuccessUrl("/login").permitAll()
-        .and()
+		http.authorizeRequests().antMatchers("/", "/student/save", "/student/showFormForAdd", "/student/403")
+				.hasAnyAuthority("USER", "ADMIN").antMatchers("/student/showFormForUpdate", "/student/delete")
+				.hasAuthority("ADMIN").anyRequest().authenticated().and().formLogin().loginProcessingUrl("/login")
+				.successForwardUrl("/student/list").permitAll().and().logout().logoutSuccessUrl("/login").permitAll()
+				.and()
 //        .exceptionHandling().accessDeniedPage("/student/403")
-        .exceptionHandling().accessDeniedPage("/student/403")
-        .and()
-        .cors().and().csrf().disable();
+				.exceptionHandling().accessDeniedPage("/student/403").and().cors().and().csrf().disable();
 	}
 
 }
